a27e3fd11987531cbd7caac0e5b9b00828036702
Gabi Moldovan <68482473+GabiMoldovan@users.noreply.github.com>
2025-11-30 13:54:32 +0200

    pythonic: added type hint

--- service/websiteService.py
+++ service/websiteService.py
@@ -1,17 +1,22 @@
+from sqlalchemy import Integer
+
+from model import Website, Product
+
+
 class WebsiteService:
     def __init__(self,
                  websiteRepository,
                  ):
         self.__websiteRepository = websiteRepository
 
-    def getWebsiteById(self, websiteId):
+    def getWebsiteById(self, websiteId: Integer) -> Website|None:
         return self.__websiteRepository.getWebsiteById(websiteId)
 
-    def getWebsiteByName(self, websiteName):
+    def getWebsiteByName(self, websiteName: str) -> Website|None:
         return self.__websiteRepository.getWebsiteByName(websiteName)
 
-    def createWebsite(self, website):
+    def createWebsite(self, website: Website) -> None:
         self.__websiteRepository.createWebsite(website)
 
-    def addProduct(self, product):
+    def addProduct(self, product: Product) -> None:
         self.__websiteRepository.addProduct(product)
